0a776e7cfde048de18ef0634ae22a734e6deb96f
isa <maria.murilloquintana@ucr.ac.cr>
2019-09-28 19:21:11 -0600

    nuevo proyecto con splash screen funcional

---
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/MainActivity.java
@@ -0,0 +1,32 @@
+package ucr.ecci.diccionariocosasmalecu;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int TIEMPO = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent actividad = new Intent(MainActivity.this, paginaPrincipal.class);
+                startActivity(actividad);
+                finish();
+            }
+        }, TIEMPO);
+    }
+}
+
+//basado en tutorial de geeksforgeeks.org/android-creating-a-splash-screen/
---
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/paginaPrincipal.java
@@ -0,0 +1,14 @@
+package ucr.ecci.diccionariocosasmalecu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paginaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagina_principal);
+    }
+}
