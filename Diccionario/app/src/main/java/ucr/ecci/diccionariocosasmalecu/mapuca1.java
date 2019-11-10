86d7dd0cce7c5fd5d9454161ff98ac52f4f174b0
isa <maria.murilloquintana@ucr.ac.cr>
2019-11-10 01:33:00 -0600

    avance botones, no salen, revisar

--- Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/mapuca1.java
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/mapuca1.java
@@ -2,7 +2,11 @@ package ucr.ecci.diccionariocosasmalecu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class mapuca1 extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class mapuca1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapuca);
+
+        ImageButton boton_atras = (ImageButton)findViewById(R.id.btn_atras);
+        ImageButton boton_adelante = (ImageButton)findViewById(R.id.btn_adelante);
+        final Context context = this;
+
+
+        //para cada botón poner hacia cuál actividad va
+        boton_atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, paginaPrincipal.class);
+                startActivity(intent);
+            }
+        });
+
+        boton_adelante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, paginaPrincipal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
