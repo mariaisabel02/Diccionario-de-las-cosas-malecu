54689128a58e4ea19384e41c82c83cea9d314c6b
Jason <jasonzatre@hotmail.com>
2019-11-12 21:49:54 -0600

    Ya la primera actividad de Jerro e inicio de Jerro2

--- Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/jerro1.java
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/jerro1.java
@@ -14,5 +14,38 @@ public class jerro1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jerro1);
+
+        ImageButton boton_atras = (ImageButton)findViewById(R.id.btn_atras);
+        ImageButton boton_adelante = (ImageButton)findViewById(R.id.btn_adelante);
+        ImageButton boton_principal = (ImageButton)findViewById(R.id.btn_principal);
+        final Context context = this;
+
+        //para cada botón poner hacia cuál actividad va
+        boton_atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, paginaPrincipal.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.izq_der, R.anim.der_izq);
+            }
+        });
+
+        boton_adelante.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, jerro2.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_der, R.anim.slide_out_izq);
+            }
+        });
+
+        boton_principal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, paginaPrincipal.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.izq_der, R.anim.der_izq);
+            }
+        });
     }
 }
---
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/jerro2.java
@@ -0,0 +1,14 @@
+package ucr.ecci.diccionariocosasmalecu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jerro2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jerro2);
+    }
+}
