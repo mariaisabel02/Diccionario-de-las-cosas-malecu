d9c8cb614c1bafaee1fe69e8035d9bee87ee9a15
isa <maria.murilloquintana@ucr.ac.cr>
2019-12-18 20:40:34 -0600

    otras cosas

---
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/curu.java
@@ -0,0 +1,30 @@
+package ucr.ecci.diccionariocosasmalecu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class curu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_curu);
+
+        ImageButton boton_principal = (ImageButton)findViewById(R.id.btn_principal);
+        final Context context = this;
+
+        boton_principal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, paginaPrincipal.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.izq_der, R.anim.der_izq);
+            }
+        });
+    }
+}
---
+++ Diccionario/app/src/main/java/ucr/ecci/diccionariocosasmalecu/quirri.java
@@ -0,0 +1,31 @@
+package ucr.ecci.diccionariocosasmalecu;
+
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class quirri extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quirri);
+        ImageButton boton_principal = (ImageButton)findViewById(R.id.btn_principal);
+        final Context context = this;
+
+        boton_principal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(context, paginaPrincipal.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.izq_der, R.anim.der_izq);
+            }
+        });
+
+
+    }
+}
